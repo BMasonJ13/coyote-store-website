550a122ee1debbee0b328609ea430fd7bbf81797
Benton Justice <61897174+BMasonJ13@users.noreply.github.com>
2021-12-31 13:12:47 -0600

    LCS Merch

--- src/pages/merch.js
+++ src/pages/merch.js
@@ -3,6 +3,7 @@ import './merch.css'
 
 import NavBar from '../components/navbar/navbar'
 import Footer from '../components/footer/Footer'
+import CoyoteLogo from '../res/svg/CoyoteLogo.svg'
 
 const Merch = () => 
 {
@@ -11,9 +12,11 @@ const Merch = () =>
         <>
             <NavBar />
             <div className="test-container">
-                <h1 className="test">COMING SOON!</h1>
+                <img className="merch-svg" src={CoyoteLogo} alt=""/>
             </div>
-            <div className="footer-stick">
+            <h1>Introducing LCS</h1>
+            <p className="merch-paragraph">The Legendary Coyote Store is proud to announce our latest lineup of accessories. Launching in Q1 of 2022, The Legendary Coyote Store will have all new hats, shirts, beverage accessories, and much more. Our 2022 merch lineup will be avaible online for the first time in our history as well as being avaible for purchase at concert venues.</p>
+            <div className="merch-footer-stick">
             <Footer />
             </div>
         </>
